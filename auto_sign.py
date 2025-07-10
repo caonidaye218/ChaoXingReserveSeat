7c1caa4e88e4afa901d5b746beb30d96cad26d22
iso665 <1356508785@qq.com>
2025-07-10 14:46:36 +0800

    Update auto_sign.py

--- auto_sign.py
+++ auto_sign.py
@@ -3,8 +3,8 @@ import time
 
 class ChaoxingAutoSign:
     def __init__(self):
-        self.username = "18873399638"
-        self.password = "Qq114514"
+        self.username = "15399756985"
+        self.password = "chenyu123"
         self.session = requests.Session()
         self.session.headers.update({
             'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
