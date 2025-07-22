183082463aa30877112d0d1497ef41e22fefe009
caonidaye218 <caonidaye218@gmail.com>
2025-07-22 09:12:41 +0800

    Update auto_sign.py

--- auto_sign.py
+++ auto_sign.py
@@ -95,7 +95,7 @@ class ChaoxingAutoSign:
 
     def run(self):
         self.login()
-        self.wait_until(target_time="08:40:01")
+        self.wait_until(target_time="09:00:00")
         time.sleep(2)
         reserves = self.get_reserve_list()
         if not reserves:
