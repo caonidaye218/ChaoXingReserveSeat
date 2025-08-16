4070f64f6ff560f8fcad9df706b6f6c0e27c68e2
iso665 <1356508785@qq.com>
2025-08-16 16:14:58 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,15 +1,16 @@
 import os 
-from .encrypt import AES_Encrypt, generate_captcha_key, enc, verify_param
+import logging
+from .encrypt import AES_Encrypt, enc, generate_behavior_analysis
 from .reserve import reserve
 
-def _fetch_env_variables(env_name, action):
-    try:
-        return os.environ[env_name] if action else ""
-    except KeyError:
-        print(f"Environment variable {env_name} is not configured correctly.")
-        return None
-
 def get_user_credentials(action):
-    usernames = _fetch_env_variables('USERNAMES', action)
-    passwords = _fetch_env_variables('PASSWORDS', action)
-    return usernames, passwords
+    """从环境变量中获取用户凭证"""
+    if not action:
+        return None, None
+    try:
+        usernames = os.environ['USERNAMES']
+        passwords = os.environ['PASSWORDS']
+        return usernames, passwords
+    except KeyError:
+        logging.error("未在 Actions Secrets 中找到 USERNAMES 或 PASSWORDS。")
+        return None, None
